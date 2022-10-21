f347c930896f2f7bb72b6e4b05a8d888a11f93f4
Георгий Евсеев <gera.evseev2001@gmail.com>
2022-10-22 02:26:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import threading
+from random import choice
+import time
+
+desktop_agents = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+
+api_addr = ['https://jsonplaceholder.typicode.com', 'http://188.127.251.4:8240']
+
+
+def random_headers():
+    return {'User-Agent': choice(desktop_agents),
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+
+
+def get_json(id_posts):
+    r = requests.get(f'{choice(api_addr)}/posts?id={id_posts}', headers=random_headers())
+    time.sleep(2)
+    # time.sleep(2)
+    if r.status_code == 200:
+        print(r.json())
+        return r.json()
+    else:
+        print('Error')
+        return {}
+
+
+if __name__ == "__main__":
+    start = time.monotonic()
+    for el in range(1, 11):
+        x = threading.Thread(target=get_json, args=(el,))
+        x.start()
+        x.join()
+
+    end = time.monotonic()
+
+    print((end - start) / 10)
